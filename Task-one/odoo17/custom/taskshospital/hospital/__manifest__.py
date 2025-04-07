8e29701d14c09c060b9958ee46806e3e11b71bfb
Ayasalem12 <aya446085@gmail.com>
2025-04-08 00:46:43 +0200

    Hospital Managament

---
+++ Task-one/odoo17/custom/taskshospital/hospital/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': 'Tasks Hospital',
+    'version': '17.0.1.0.0',
+     "sequence": -100,
+    'depends': ['base'],
+    'author': 'Aya',
+    'category': 'Custom',
+    'description': 'A custom module for hospital tasks',
+    'data': [
+        'views/patient_views.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+}
